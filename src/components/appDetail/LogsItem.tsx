d68193402c5e89ddc3c15e7808071b7faeae2bd8
0livia8899 <0livia_hu@aefinder.io>
2024-06-21 15:52:48 +0800

    fix: 🐛 fix logs display and logout display and upload check

--- src/components/appDetail/LogsItem.tsx
+++ src/components/appDetail/LogsItem.tsx
@@ -1,36 +1,69 @@
-import { DownOutlined, UpOutlined } from '@ant-design/icons';
+import { CopyOutlined, DownOutlined, UpOutlined } from '@ant-design/icons';
+import { MessageInstance } from 'antd/es/message/interface';
 import clsx from 'clsx';
 import { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 type LogsItemProps = {
   message: string;
   exception: string;
+  messageApi: MessageInstance;
 };
 
-export default function LogsItem({ message, exception }: LogsItemProps) {
+export default function LogsItem({
+  message,
+  exception,
+  messageApi,
+}: LogsItemProps) {
   const [isShow, setIsShow] = useState(false);
 
+  const handleCopy = () => {
+    messageApi.success({
+      content: 'Copied',
+      key: 'copy',
+    });
+  };
+
   return (
-    <div className='overflow-wrap relative w-full'>
-      <div
-        className='text-gray-80 absolute right-0 top-0 cursor-pointer'
-        onClick={() => setIsShow(!isShow)}
-      >
+    <div
+      className={clsx('overflow-wrap relative')}
+      style={{ width: `calc(100% - 340px)` }}
+    >
+      <div className='text-gray-80 absolute right-0 top-0 cursor-pointer'>
         {isShow ? (
-          <UpOutlined className='relative right-[-14px]' />
+          <div className='relative right-[-14px] flex flex-col'>
+            <UpOutlined
+              className='mb-[10px]'
+              onClick={() => setIsShow(!isShow)}
+            />
+            <CopyToClipboard
+              text={message + exception}
+              onCopy={() => handleCopy()}
+            >
+              <CopyOutlined />
+            </CopyToClipboard>
+          </div>
         ) : (
-          <DownOutlined className='relative right-[-14px]' />
+          <DownOutlined
+            className='relative right-[-14px]'
+            onClick={() => setIsShow(!isShow)}
+          />
         )}
       </div>
       <div
         className={clsx(
           'w-full max-w-[100%] break-all text-left',
-          isShow ? 'truncate-10-lines' : 'truncate-3-lines'
+          isShow ? 'truncate-10-lines' : 'line-clamp-3'
         )}
       >
         {message}
       </div>
-      <div className='text-muted w-full max-w-[100%] truncate break-all text-left text-black'>
+      <div
+        className={clsx(
+          'w-full max-w-[100%] break-all text-left font-bold text-black',
+          isShow ? 'truncate-10-lines' : 'line-clamp-1'
+        )}
+      >
         {exception}
       </div>
     </div>
