129d50522b5f3f00b86bd21a6b0d2cda4fd92769
0livia8899 <0livia_hu@aefinder.io>
2024-07-05 18:32:03 +0800

    fix: 🐛 change code as SonarQube rules

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -1,4 +1,4 @@
 export const isProd = process.env.NODE_ENV === 'production';
 export const isLocal = process.env.NODE_ENV === 'development';
-const isShow = process.env.NEXT_PUBLIC_SHOW_LOGGER || 'false';
-export const showLogger = isLocal ? true : isShow === 'true' ?? false;
+
+export const showLogger = isLocal ? true : process.env.NEXT_PUBLIC_SHOW_LOGGER;
