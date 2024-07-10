6be9e9baa55506f4db31aa70e2b572a777a322da
0livia8899 <0livia_hu@aefinder.io>
2024-07-10 16:58:30 +0800

    feat: 🎸 deploy page adapt to h5 px

--- src/components/appDetail/DetailBox.tsx
+++ src/components/appDetail/DetailBox.tsx
@@ -1,3 +1,4 @@
+import { Col, Row } from 'antd';
 import dayjs from 'dayjs';
 
 import Copy from '@/components/Copy';
@@ -11,51 +12,53 @@ type DetailBoxProps = {
 export default function DetailBox({ currentAppDetail }: DetailBoxProps) {
   return (
     <div className='bg-gray-F5 mt-[30px] flex w-full items-start justify-start rounded-md px-[20px] py-[30px]'>
-      <div className='w-[49%] max-w-[50%] pr-[24px]'>
-        <div className='text-block mb-[24px] text-xl font-medium'>
-          {currentAppDetail?.appName}
-        </div>
-        <div className='mb-[24px]'>
-          <Copy label='Network' content='aelf' />
+      <Row gutter={24} className='w-full'>
+        <Col sm={24} md={12} className='min-w-[360px]'>
+          <div className='text-block mb-[24px] text-xl font-medium'>
+            {currentAppDetail?.appName}
+          </div>
+          <div className='mb-[24px] flex justify-between'>
+            <Copy label='Network' content='aelf' />
+            <Copy
+              className='mx-[32px]'
+              label='Last updated'
+              content={dayjs(currentAppDetail?.updateTime).format('YYYY-MM-DD')}
+            />
+            <Copy
+              label='Created'
+              content={dayjs(currentAppDetail?.createTime).format('YYYY-MM-DD')}
+            />
+          </div>
+          {currentAppDetail?.description && (
+            <Copy
+              label='Description'
+              content={currentAppDetail?.description}
+              className='relative mb-4 w-[80%] overflow-hidden'
+            />
+          )}
+        </Col>
+        <Col sm={24} md={12} className='flex flex-col'>
           <Copy
-            className='mx-[32px]'
-            label='Last updated'
-            content={dayjs(currentAppDetail?.updateTime).format('YYYY-MM-DD')}
-          />
-          <Copy
-            label='Created'
-            content={dayjs(currentAppDetail?.createTime).format('YYYY-MM-DD')}
-          />
-        </div>
-        {currentAppDetail?.description && (
-          <Copy
-            label='Description'
-            content={currentAppDetail?.description}
-            className='relative w-[80%] overflow-hidden'
-          />
-        )}
-      </div>
-      <div className='flex w-[50%] max-w-[50%] flex-col'>
-        <Copy
-          label='AppID'
-          content={currentAppDetail?.appId}
-          isShowCopy={true}
-        />
-        <Copy
-          className='my-[24px]'
-          label='Deploy Key'
-          content={currentAppDetail?.deployKey ?? ''}
-          isShowCopy={true}
-          showLittle={true}
-        />
-        {currentAppDetail?.sourceCodeUrl && (
-          <Copy
-            label='SourceCodeUrl'
-            content={currentAppDetail?.sourceCodeUrl}
+            label='AppID'
+            content={currentAppDetail?.appId}
             isShowCopy={true}
           />
-        )}
-      </div>
+          <Copy
+            className='my-[24px]'
+            label='Deploy Key'
+            content={currentAppDetail?.deployKey ?? ''}
+            isShowCopy={true}
+            showLittle={true}
+          />
+          {currentAppDetail?.sourceCodeUrl && (
+            <Copy
+              label='SourceCodeUrl'
+              content={currentAppDetail?.sourceCodeUrl}
+              isShowCopy={true}
+            />
+          )}
+        </Col>
+      </Row>
     </div>
   );
 }
