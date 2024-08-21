d0e4fdde6da778509ed969db4ed56edddd710af4
0livia8899 <0livia_hu@aefinder.io>
2024-08-21 18:00:01 +0800

    feat: 🎸 add Opentelemetry trace api

---
+++ src/components/layout/opentelemetryProvider.tsx
@@ -0,0 +1,53 @@
+'use client';
+import { Tracer } from '@opentelemetry/sdk-trace-base';
+import { initWebTracerWithZone } from 'opentelemetry-launcher';
+import { createContext, ReactNode, useEffect, useMemo, useState } from 'react';
+
+import { CollectorEndpoint } from '@/constant';
+
+interface Props {
+  readonly children: ReactNode;
+}
+
+export const OpentelemetryContext = createContext<Tracer | undefined>(
+  undefined
+);
+
+export const OpentelemetryProvider = ({ children }: Props) => {
+  const { NEXT_PUBLIC_NETWORK_KEY } = process.env;
+  const isMainNet = NEXT_PUBLIC_NETWORK_KEY === 'mainnet';
+  const prefix = isMainNet ? 'mainnet' : 'testnet';
+  const [webTracerWithZone, setWebTracerWithZone] = useState<Tracer>();
+  const APP_SETTINGS = useMemo(() => {
+    return {
+      openTelemetry: {
+        serviceName: 'aefinder-web-' + prefix,
+        serviceVersion: '2.14.0',
+        collectorEndpoint: CollectorEndpoint,
+        tracerName: `aefinder-web-${prefix}-tracer`,
+        ignoreUrls: [
+          /\/sockjs-node/,
+          /\/monitoring/,
+          /\/__nextjs_original-stack-frame/,
+          /_rsc=/,
+          /https:\/\/www\.google-analytics\.com\/g\/collect/,
+        ],
+        propagateTraceHeaderCorsUrls: ['https://httpbin.org'],
+      },
+    };
+  }, [prefix]);
+
+  useEffect(() => {
+    const _webTracerWithZone = initWebTracerWithZone(
+      APP_SETTINGS.openTelemetry
+    );
+    setWebTracerWithZone(_webTracerWithZone);
+    console.log('getWebTracerWithZone done');
+  }, [APP_SETTINGS.openTelemetry]);
+
+  return (
+    <OpentelemetryContext.Provider value={webTracerWithZone}>
+      {children}
+    </OpentelemetryContext.Provider>
+  );
+};
--- src/constant/index.ts
+++ src/constant/index.ts
@@ -4,26 +4,35 @@ const networkHostMap = {
   localnet: {
     AeFinderHost: '',
     AeFinderAuthHost: '',
+    CollectorEndpoint: '',
   },
   devnet: {
     AeFinderHost: 'http://192.168.71.128:8081',
     AeFinderAuthHost: 'http://192.168.71.128:8082',
+    CollectorEndpoint: '',
   },
   testnet: {
     AeFinderHost: 'https://gcptest-indexer-api.aefinder.io',
     AeFinderAuthHost: 'https://gcptest-indexer-auth.aefinder.io',
+    CollectorEndpoint:
+      'http://otel-collector-testnet-collector.observability:4316',
   },
   mainnet: {
     AeFinderHost: 'https://indexer-api.aefinder.io',
     AeFinderAuthHost: 'https://indexer-auth.aefinder.io',
+    CollectorEndpoint: '',
   },
 };
 
 export type networkType = 'mainnet' | 'testnet' | 'devnet' | 'localnet';
 export const AeFinderHost =
   networkHostMap[network as networkType]?.AeFinderHost;
+
 export const AeFinderAuthHost =
   networkHostMap[network as networkType]?.AeFinderAuthHost;
+
+export const CollectorEndpoint =
+  networkHostMap[network as networkType]?.CollectorEndpoint;
 
 export const LogsColor = {
   Debug: '#1890ff',
