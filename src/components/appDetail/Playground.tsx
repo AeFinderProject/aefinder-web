42c2a1926c4a6e3b0a57681d45e456fced7fc9a4
0livia8899 <0livia_hu@aefinder.io>
2024-09-18 18:43:21 +0800

    feat: 🎸 test graphql url apply cross origin

--- src/components/appDetail/Playground.tsx
+++ src/components/appDetail/Playground.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { createGraphiQLFetcher } from '@graphiql/toolkit';
+// import { createGraphiQLFetcher } from '@graphiql/toolkit';
 import { GraphiQL } from 'graphiql';
 import { useCallback } from 'react';
 
@@ -8,31 +8,42 @@ import 'graphiql/graphiql.css';
 
 import { useAppSelector } from '@/store/hooks';
 
-import { appApiList } from '@/api/list';
-
 import GuestPlayground from './GuestPlayground/GuestPlayground';
 
 export default function Playground() {
-  const { currentAppDetail, currentVersion } = useAppSelector(
-    (state) => state.app
-  );
-  const { appId } = currentAppDetail;
+  const { currentVersion } = useAppSelector((state) => state.app);
   const isGuest = sessionStorage.getItem('isGuest');
 
   const getGraphiqlhUI = useCallback(() => {
     // when currentVersion is null, it means the app is not deployed
     if (!currentVersion) return;
-    const tempFetcher = createGraphiQLFetcher({
-      url: `${appApiList?.fetchQraphiql?.target}/${appId}/${currentVersion}`,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    // const tempFetcher = createGraphiQLFetcher({
+    //   url: `https://app-testnet.aefinder.io/wahaha0918/754ad75b47294dc5ba0dcac06110d839/graphql`,
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    // });
+    // eslint-disable-next-line
+    const graphQLFetcher = async (graphQLParams: any) => {
+      const response = await fetch(
+        'https://app-testnet.aefinder.io/wahaha0918/754ad75b47294dc5ba0dcac06110d839/graphql',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(graphQLParams),
+        }
+      );
+
+      return response.json();
+    };
+
     localStorage.removeItem('graphiql:query');
     localStorage.removeItem('graphiql:tabState');
     localStorage.setItem('graphiql:theme', 'light');
-    return <GraphiQL fetcher={tempFetcher} editorTheme='Light' />;
-  }, [appId, currentVersion]);
+    return <GraphiQL fetcher={graphQLFetcher} editorTheme='Light' />;
+  }, [currentVersion]);
 
   return (
     <div id='graphiql-box' className='relative h-[756px] w-full'>
