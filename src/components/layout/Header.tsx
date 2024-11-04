c794571e7ffbde921394c9add683eaccaec2dae7
0livia8899 <0livia_hu@aefinder.io>
2024-11-04 18:49:07 +0800

    fix: 🐛 set address as  '' when logout

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -85,6 +85,7 @@ export default function Header() {
     if (isConnected) {
       await disConnectWallet();
     }
+    setAddress('');
     router.push('/login');
   }, [router, isConnected, disConnectWallet]);
 
