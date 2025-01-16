e9aa328d33b2a29977ec841306ede7530fc8bd6a
0livia8899 <0livia_hu@aefinder.io>
2025-01-16 12:37:57 +0800

    fix: 🐛 add contact us when org deactived

--- src/components/appDetail/DeactivatedModal.tsx
+++ src/components/appDetail/DeactivatedModal.tsx
@@ -3,6 +3,10 @@ import { MessageInstance } from 'antd/es/message/interface';
 import Image from 'next/image';
 import { useCallback } from 'react';
 
+import { openWithBlank } from '@/lib/utils';
+
+import { useAppSelector } from '@/store/hooks';
+
 type DeactivatedModalProps = {
   readonly isShowDeactivatedModal: boolean;
   readonly setIsShowDeactivatedModal: (visible: boolean) => void;
@@ -15,6 +19,8 @@ export default function DeactivatedModal({
   setIsShowDeactivatedModal,
   setIsShowUpdateCapacityModal,
 }: DeactivatedModalProps) {
+  const orgUser = useAppSelector((state) => state.common.orgUser);
+
   const handleClose = useCallback(() => {
     setIsShowDeactivatedModal(false);
   }, [setIsShowDeactivatedModal]);
@@ -46,20 +52,32 @@ export default function DeactivatedModal({
       <div className='text-gray-80'>
         Your AeIndexer is currently deactivated due to insufficient balance. To
         reactivate your AeIndexer and continue enjoying our services, please add
-        funds to your Balance.
+        funds to your Balance or contact us.
       </div>
       <div className='mt-[24px] flex justify-between'>
         <Button className='w-[48%]' onClick={handleClose} size='large'>
           Cancel
         </Button>
-        <Button
-          className='w-[48%]'
-          onClick={handleShowUpdateCapacity}
-          size='large'
-          type='primary'
-        >
-          Set Capacity
-        </Button>
+        {orgUser?.organizationStatus === 1 && (
+          <Button
+            className='w-[48%]'
+            onClick={() => openWithBlank('https://form.aefinder.io/contact')}
+            size='large'
+            type='primary'
+          >
+            Contact us
+          </Button>
+        )}
+        {orgUser?.organizationStatus === 0 && (
+          <Button
+            className='w-[48%]'
+            onClick={handleShowUpdateCapacity}
+            size='large'
+            type='primary'
+          >
+            Set Capacity
+          </Button>
+        )}
       </div>
     </Modal>
   );
