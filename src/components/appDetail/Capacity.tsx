583e77bb561729b40fe3bfabc77aef8653cc855f
0livia8899 <0livia_hu@aefinder.io>
2025-03-26 10:50:45 +0800

    fix: 🐛 change disk display rule

--- src/components/appDetail/Capacity.tsx
+++ src/components/appDetail/Capacity.tsx
@@ -91,63 +91,62 @@ export default function Capacity() {
       {(!podUsage || podUsage?.length === 0) && (
         <div className='text-gray-80 text-center'>No Data</div>
       )}
-      {podUsage &&
-        podUsage?.length > 0 &&
-        podUsage?.map((item, index) => {
-          return (
-            <div key={index}>
-              <div className='flex items-center justify-between'>
-                <div>AeIndexer Capacity</div>
-                <div className='text-gray-80 text-xs'>{item?.currentState}</div>
-              </div>
-              <div className='mt-[14px] flex items-center justify-between gap-[14px]'>
-                <div className='border-gray-E0 flex-1 rounded-lg border p-[24px]'>
-                  <div className='text-gray-80'>CPU</div>
-                  <div className='text-dark-normal mb-[6px] mt-[8px] font-medium'>
-                    {displayCapacity()?.currentCpuUsage} /{' '}
-                    {displayCapacity()?.currentCpuLimit}
-                  </div>
-                  <Progress
-                    percent={calcDiv100(
-                      Number(displayCapacity()?.currentCpuUsage),
-                      Number(displayCapacity()?.currentCpuLimit)
-                    )}
-                    showInfo={false}
-                  />
-                </div>
-                <div className='border-gray-E0 flex-1 rounded-lg border p-[24px]'>
-                  <div className='text-gray-80'>RAM</div>
-                  <div className='text-dark-normal mb-[6px] mt-[8px] font-medium'>
-                    {displayCapacity()?.currentMemoryUsage} /{' '}
-                    {displayCapacity()?.currentMemoryLimit}
-                  </div>
-                  <Progress
-                    percent={calcDiv100(
-                      displayCapacity()?.currentMemoryUsage,
-                      displayCapacity()?.currentMemoryLimit
-                    )}
-                    showInfo={false}
-                  />
-                </div>
-                {storageUsage && (
-                  <div className='border-gray-E0 flex-1 rounded-lg border p-[24px]'>
-                    <div className='text-gray-80'>Disk</div>
-                    <div className='text-dark-normal mb-[6px] mt-[8px] font-medium'>
-                      {storageUsage?.usage} / {storageUsage?.limit}
-                    </div>
-                    <Progress
-                      percent={calcDiv100(
-                        storageUsage?.usage,
-                        storageUsage?.limit
-                      )}
-                      showInfo={false}
-                    />
-                  </div>
-                )}
-              </div>
+      {((podUsage && podUsage?.length > 0) || storageUsage) && (
+        <div>
+          <div className='flex items-center justify-between'>
+            <div>AeIndexer Capacity</div>
+            <div className='text-gray-80 text-xs'>
+              {(podUsage && podUsage[0]?.currentState) || '--'}
             </div>
-          );
-        })}
+          </div>
+          <div className='mt-[14px] flex items-center justify-between gap-[14px]'>
+            {podUsage && podUsage?.length > 0 && (
+              <div className='border-gray-E0 flex-1 rounded-lg border p-[24px]'>
+                <div className='text-gray-80'>CPU</div>
+                <div className='text-dark-normal mb-[6px] mt-[8px] font-medium'>
+                  {displayCapacity()?.currentCpuUsage} /{' '}
+                  {displayCapacity()?.currentCpuLimit}
+                </div>
+                <Progress
+                  percent={calcDiv100(
+                    Number(displayCapacity()?.currentCpuUsage),
+                    Number(displayCapacity()?.currentCpuLimit)
+                  )}
+                  showInfo={false}
+                />
+              </div>
+            )}
+            {podUsage && podUsage?.length > 0 && (
+              <div className='border-gray-E0 flex-1 rounded-lg border p-[24px]'>
+                <div className='text-gray-80'>RAM</div>
+                <div className='text-dark-normal mb-[6px] mt-[8px] font-medium'>
+                  {displayCapacity()?.currentMemoryUsage} /{' '}
+                  {displayCapacity()?.currentMemoryLimit}
+                </div>
+                <Progress
+                  percent={calcDiv100(
+                    displayCapacity()?.currentMemoryUsage,
+                    displayCapacity()?.currentMemoryLimit
+                  )}
+                  showInfo={false}
+                />
+              </div>
+            )}
+            {storageUsage && (
+              <div className='border-gray-E0 flex-1 rounded-lg border p-[24px]'>
+                <div className='text-gray-80'>Disk</div>
+                <div className='text-dark-normal mb-[6px] mt-[8px] font-medium'>
+                  {storageUsage?.usage} / {storageUsage?.limit} GB
+                </div>
+                <Progress
+                  percent={calcDiv100(storageUsage?.usage, storageUsage?.limit)}
+                  showInfo={false}
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
